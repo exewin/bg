b3a37086b80ef4682f4dc1195282af5bb3f4cd7d
Łukasz Jasiński <windowsmemphis@gmail.com>
2022-06-23 19:43:43 +0200

    add background effect

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,13 +1,24 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css, keyframes } from 'styled-components'
+
+
+const a = keyframes`
+  from {opacity: 0;}
+  to {opacity: 1;}
+`
+
+const s = css`
+animation: ${a} 0.8s;
+`
 
 const Div = styled.div` 
-background-image: url(${props=>props.bg});
-background-repeat: no-repeat;
-background-size: cover;
-height: calc(100% - 4px);
-width: calc(100% - 4px);
-padding: 2px;
+  background-image: url(${props=>props.bg});
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: calc(100% - 4px);
+  width: calc(100% - 4px);
+  padding: 2px;
+  ${s};
 `
 
 
