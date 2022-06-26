72aacd4d1263e97ee2fab8812735b674ba8b7376
Łukasz Jasiński <windowsmemphis@gmail.com>
2022-06-26 16:46:19 +0200

    rework backgrounds

---
+++ src/utils/backgroundController.js
@@ -0,0 +1,6 @@
+import p0 from "../assets/backgrounds/0.jpg"
+import p1 from "../assets/backgrounds/1.jpg"
+import p2 from "../assets/backgrounds/2.jpg"
+import p3 from "../assets/backgrounds/3.jpg"
+
+export const bgs = [p0,p1,p2,p3]
