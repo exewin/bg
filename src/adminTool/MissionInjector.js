ce282582f5fc2e7e3b71f77389b53bd9cd6f19f0
Łukasz Jasiński <windowsmemphis@gmail.com>
2022-06-27 21:40:12 +0200

    update injector

--- src/adminTool/MissionInjector.js
+++ src/adminTool/MissionInjector.js
@@ -5,7 +5,7 @@ import { missions } from '../firebase/missionTemplate'
 export const MissionInjector = () => {
 
     const injectMission = () =>{
-        addMissionDB(missions[0],"3")
+        addMissionDB(missions[0],"4")
     }
 
     return (
--- src/firebase/missionTemplate.js
+++ src/firebase/missionTemplate.js
@@ -1,5 +1,14 @@
 export const missions = [
     {
+        //0
+        name: "Ancient Knowledge",
+        description:"I've been expecting you %n. You must go to mystic chamber and find scroll of truth. It holds great power and can't fall into our enemy hands.",
+        xp: 600,
+        gold: 5,
+        time: 40,
+    },    
+    {
+        //1
         name: "Money for Nothing",
         description:"What? You are looking for quest? Ok, You will travel to my friend in swamp and pick up some frog oil from him. Give him this purse in return. Also, don't ask any questions.",
         xp: 333,
