c6e97b697af12670fcd16cc8ba7691cf24af01da
Łukasz Jasiński <windowsmemphis@gmail.com>
2022-06-29 14:37:10 +0200

    add level up function

---
+++ src/logic/CharacterLevelUp.js
@@ -0,0 +1,13 @@
+export const levelUp = (character) => {
+    if(!character) return "error"
+    if(character.stats.xp >= character.stats.maxXp){
+        console.log("character level up")
+        character.stats.xp -= character.stats.maxXp
+        character.stats.level += 1
+        character.stats.maxXp = (character.stats.level*1000)
+        levelUp(character)
+    }
+    else {
+        return
+    }
+}
