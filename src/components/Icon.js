c6c77f7470246acfe5deb3156f22df99180346e5
Łukasz Jasiński <windowsmemphis@gmail.com>
2022-06-30 22:57:27 +0200

    add icon component

---
+++ src/components/Icon.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import palIcon from "../assets/icons/paladin.png"
+import warIcon from "../assets/icons/warrior.png"
+import magIcon from "../assets/icons/mage.png"
+import styled from 'styled-components'
+
+const Img = styled.img` 
+width:48px;
+border-radius: 4px;
+border: ${props=>props.selected ? '2px solid green' : '2px solid black'};
+&:hover{
+    border: ${props=>props.selected ? '2px solid lime' : '2px solid yellow'};
+    cursor:pointer;
+}
+`
+
+const icons = [warIcon, palIcon, magIcon]
+
+export const Icon = ({onClick, icon, selected}) => {
+  return (
+    <Img 
+        src={icons[icon]} 
+        onClick={onClick} 
+        selected={selected}
+    />
+  )
+}
