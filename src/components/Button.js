c01fbf3907efdca3ddb03e00a23d944614baafbe
Łukasz Jasiński <windowsmemphis@gmail.com>
2022-06-30 22:56:56 +0200

    expand button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import styled from 'styled-components'
-import imagea from "../assets/ui/button.png"
+import btn from "../assets/ui/button.png"
+import wbtn from "../assets/ui/high_button.png"
 
 const Div=styled.div` 
 background-image: url(${props=>props.bg});
 background-size: cover;
-width:164px;
-height:27px;
+width:${props=>props.w}px;
+height:${props=>props.h}px;
 display:flex;
 align-items: center;
 justify-content: center;
 align-self: center;
 cursor: pointer;
 user-select: none;
+${props=>props.css};
 `
 
-export const Button = ({children, onClick}) => {
+export const Button = ({children, onClick, wide = false, css, size=1}) => {
+  const width = 164*size
+  const height = wide ? 54*size : 27*size;
   return (
-    <Div bg={imagea} onClick={onClick}>{children}</Div>
+    <Div bg={wide ? wbtn : btn} w={width} h={height} css={css} onClick={onClick}>{children}</Div>
   )
 }
