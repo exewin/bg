47f5317744a71d2390991572a8c0d6bd06011027
Łukasz Jasiński <windowsmemphis@gmail.com>
2022-07-01 15:04:04 +0200

    enchance creation screen

--- src/sites/dashboard/Creation.js
+++ src/sites/dashboard/Creation.js
@@ -1,7 +1,5 @@
-import { Box, Button, ButtonGroup, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import {ArrowCircleLeft, ArrowCircleRight } from '@mui/icons-material'
 import { Portrait } from '../../components/Portrait'
 import { selectPortrait } from '../../utils/portraitController'
 import { createCharacterDB, userExistsDB } from '../../firebase/firestore'
@@ -10,26 +8,78 @@ import { useNavigate } from 'react-router-dom'
 import { CenteredLoading } from '../../components/CenteredLoading'
 import { Background } from '../../components/Background'
 import { bgs } from '../../utils/backgroundController'
+import card from "../../assets/ui/missionCard.png"
+import { Box } from '../../components/Box'
+import { Button } from '../../components/Button'
+import { Icon } from '../../components/Icon'
+import { classDescs } from '../../utils/classDescription'
+import { capitalizeWord } from '../../utils/capitalizeWord'
+import { nameRegex } from '../../utils/regExps'
 
 
 const PortraitBox = styled.div`
 width:200px;
 `
 
+const Title = styled.h3``
+const Description = styled.p`
+text-align: left;
+font-size: 0.8em;
+`
+
+const Container = styled.div` 
+height: 90vh;
+width:100%;
+overflow-y: auto;
+overflow-x: hidden;
+`
+
+const Input = styled.input` 
+height:25px;
+width: 95%;
+`
+
+const Wrapper = styled.div` 
+display:flex;
+align-items:center;
+justify-content: center;
+`
+
+const IconWrapper = styled.div` 
+display:flex;
+align-items:center;
+justify-content: center;
+gap:2px;
+`
+
+const Warning = styled.div` 
+color:#f99;
+font-size: 12px;
+`
+
 export const Creation = () => {
 
     const [portrait, setPortrait] = useState(0)
     const [name, setName] = useState("")
+    const [nameWarning, setNameWarning] = useState("")
     const [charClass, setCharClass] = useState("warrior")
     const {user} = useAuth()
     const [loading, setLoading] = useState(true)
     const navigate = useNavigate()
 
+
     const handleSubmit = async (e) => {
         e.preventDefault()
-        setLoading(true)
-        await createCharacterDB(user.uid, {name, portrait, charClass})
-        navigate("../gamescreen")
+        if(!nameRegex.test(name)){
+            return setNameWarning("Name must be 3-12 characters long and can contain only English letters.")
+        }
+
+        try{
+            await createCharacterDB(user.uid, {name, portrait, charClass})
+            navigate("../gamescreen")
+        } catch (e) {
+            setNameWarning(e.message)
+        }
     }
 
     useEffect(()=>{
@@ -42,64 +92,58 @@ export const Creation = () => {
         setLoading(false)
     },[user])
 
-
-
     return (
-        <Background img={bgs[3]}>
-        <Box
-        sx={{
-            borderRadius: '5px',
-            padding: '10px',
-            width: '300px',
-            height: '600px',
-            backgroundColor: 'rgba(177,123,255,0.9)',
-        }}>
+        <Container>
+            <Background img={bgs[3]} css={{position:'relative'}}>
             {loading ? <CenteredLoading/> : 
-            <>
-                <Typography variant="subtitle1">
-                    Create character:
-                </Typography>
-                <form onSubmit={handleSubmit}>
-                    <FormControl>
-
-                        <TextField 
-                            required
-                            name="name"
-                            label="Name" 
-                            type="name" 
-                            autoComplete="off"
-                            size="small" 
-                            variant="filled"
-                            value={name}
-                            onChange={(e)=>setName(e.target.value)}
-                        />
-
-                        <PortraitBox>
-                            <Portrait index={portrait}/>
-                            <ButtonGroup variant="contained">
-                                <Button onClick={()=>setPortrait(prev=>selectPortrait(prev+1))} sx={{width:'100px'}}><ArrowCircleLeft/></Button>
-                                <Button onClick={()=>setPortrait(prev=>selectPortrait(prev-1))} sx={{width:'100px'}}><ArrowCircleRight/></Button>
-                            </ButtonGroup>
-                        </PortraitBox>
-
-                        <FormLabel sx={{ mt: 1 }}>Class</FormLabel>
-                        <RadioGroup 
-                            defaultValue={charClass} 
-                            name="radio-class"
-                            value={charClass}
-                            onChange={(e)=>setCharClass(e.target.value)}
-                        >
-                            <FormControlLabel value="warrior" control={<Radio />} label="Warrior" />
-                            <FormControlLabel value="paladin" control={<Radio />} label="Paladin" />
-                            <FormControlLabel value="mage" control={<Radio />} label="Mage" />
-                        </RadioGroup>
-
-                    </FormControl>
-                    <div><Button sx={{ mt: 1 }} variant="contained" type="submit">Ready</Button></div>
-                </form>
-            </>
+                    <>
+                        <Box bg={card} scale={1}>
+                            <Title>
+                                Create Character:
+                            </Title>
+                                <Input
+                                    required
+                                    name="name"
+                                    placeholder="Name" 
+                                    type="name" 
+                                    autoComplete="off"
+                                    value={name}
+                                    onChange={(e)=>setName(capitalizeWord(e.target.value))}
+                                />
+                                {nameWarning && <Warning>{nameWarning}</Warning>}
+                                <PortraitBox>
+                                    <Portrait index={portrait}/>
+                                        <Wrapper>
+                                            <Button wide size={0.5} onClick={()=>setPortrait(prev=>selectPortrait(prev+1))}>(---</Button>
+                                            <Button wide size={0.5} onClick={()=>setPortrait(prev=>selectPortrait(prev-1))}>---)</Button>
+                                        </Wrapper>
+                                </PortraitBox>
+                                    
+                        </Box>
+                        <Box scale={0.8} bg={card}>
+                            <Title>
+                                {capitalizeWord(charClass)}
+                            </Title>
+                            <IconWrapper>
+                                <Icon selected={charClass === 'warrior'} icon={0} onClick={()=>setCharClass("warrior")}/>
+                                <Icon selected={charClass === 'paladin'} icon={1} onClick={()=>setCharClass("paladin")}/>
+                                <Icon selected={charClass === 'mage'} icon={2} onClick={()=>setCharClass("mage")}/>
+                            </IconWrapper>
+                            {classDescs(charClass)}
+                            <Button onClick={handleSubmit}>Ready</Button>
+                        </Box>
+                    </>
             }
-        </Box>
-        </Background>
+            </Background>
+        </Container>
     )
 }
+
+
+
+// <RadioGroup 
+// defaultValue={charClass} 
+// name="radio-class"
+// value={charClass}
+// onChange={(e)=>setCharClass(e.target.value)}
+// >
