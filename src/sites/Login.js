a1243de68a71d8a1b6741e8aa05d27ed9207d25d
Łukasz Jasiński <windowsmemphis@gmail.com>
2022-07-01 23:24:56 +0200

    update login site

--- src/sites/Login.js
+++ src/sites/Login.js
@@ -4,9 +4,9 @@ import {LoginBox} from "./../components/LoginBox"
 import {RegisterBox} from "./../components/RegisterBox"
 import {Background} from "./../components/Background"
 import bg from "../assets/backgrounds/5.jpg"
+import {Footer} from "./../components/Footer"
 
 const Container = styled.div`
-background-color: azure;
 height: 100vh;
 max-width: 1200px;
 margin: 0 auto;
@@ -19,18 +19,20 @@ justify-content:center;
 align-items:center;
 `
 
-const Title = styled.h3` 
+const Title = styled.h1` 
+text-align: center;
 `
 
 export const Login = () => {
     return(
         <Container>
-            <Background img={bg}>
-                <Title>welcome guys</Title>
+            <Background img={bg} css={{position:'relative'}}>
+                <Title>Enter the game</Title>
                 <Wrapper>
                     <LoginBox/>
                     <RegisterBox/> 
                 </Wrapper>
+                <Footer css={{position:'absolute', bottom:'0px', marginTop:"20px", width:'90%', textAlign:'center'}}/>
             </Background>
         </Container>
     )
