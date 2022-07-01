85e78dd3d2eb328fe8066286e9716942ac59ba35
Łukasz Jasiński <windowsmemphis@gmail.com>
2022-07-01 18:27:13 +0200

    add character template for new characters

---
+++ src/logic/CharacterTemplate.js
@@ -0,0 +1,25 @@
+export const getStartVariables = {
+    progress: {
+        busy: false,
+        taskEnd: null,
+        taskId: "",
+        taskStart: null,
+        taskTime: 0,
+        type: "",
+    },
+    stats: {
+        dexCost: 1,
+        dexterity: 10,
+        endCost: 1,
+        endurance: 10,
+        level: 1,
+        maxXp: 1000,
+        money: 10,
+        strCost: 1,
+        strength: 10,
+        xp: 0,
+    },
+    mails: [
+        {author: "King", msg: "Welcome", read: false}
+    ]
+}
