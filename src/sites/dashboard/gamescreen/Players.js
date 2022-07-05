5acf1adcc52c8dd7fe822439e433ed52a347d8d1
Łukasz Jasiński <windowsmemphis@gmail.com>
2022-07-05 22:44:11 +0200

    add players site

--- src/sites/dashboard/gamescreen/Players.js
+++ src/sites/dashboard/gamescreen/Players.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import { Background } from '../../../components/Background'
 import { Button } from '../../../components/Button'
 import { bgs } from '../../../utils/backgroundController'
+import {findUserByNameDB} from "../../../firebase/firestore"
 
 const Input = styled.input` 
 height:23px;
@@ -21,19 +22,29 @@ export const Players = () => {
     const [playerInput, setPlayerInput] = useState("")
     const [playerInfo, setPlayerInfo] = useState(null)
 
-  return (
-    <Background img={bgs[2]}>
-        <Container>
-            <Wrapper>
-                <Input
-                    onChange={(e)=>setPlayerInput(e.target.value)} 
-                    value={playerInput}
-                    placeholder="find player..." 
-                    type="input"
-                />
-                <Button>Find</Button>
-            </Wrapper>
-        </Container>
-    </Background>
-  )
+    const findPlayer = async() => {
+        const response = await findUserByNameDB(playerInput)
+        if(response)
+            setPlayerInfo(response)
+        else
+            console.log("errr")
+    }
+
+    console.log(playerInfo)
+
+    return (
+        <Background img={bgs[2]}>
+            <Container>
+                <Wrapper>
+                    <Input
+                        onChange={(e)=>setPlayerInput(e.target.value)} 
+                        value={playerInput}
+                        placeholder="find player..." 
+                        type="input"
+                    />
+                    <Button onClick={findPlayer}>Find</Button>
+                </Wrapper>
+            </Container>
+        </Background>
+    )
 }
