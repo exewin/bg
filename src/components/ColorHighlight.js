076b34bbd79cace4562270006e7af806ee7050fa
Łukasz Jasiński <windowsmemphis@gmail.com>
2022-07-05 14:02:39 +0200

    extend color highlight

--- src/components/ColorHighlight.js
+++ src/components/ColorHighlight.js
@@ -3,10 +3,11 @@ import styled from 'styled-components'
 
 const Color = styled.span`
 color: ${props=>props.light === props.match ? 'yellow' : 'white'};
+${props=>props.css};
 `
 
-export const ColorHighlight = ({children, light, match}) => {
+export const ColorHighlight = ({children, light, match, css}) => {
   return (
-    <Color light={light} match={match}>{children}</Color>
+    <Color light={light} match={match} css={css}>{children}</Color>
   )
 }
