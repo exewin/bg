6fedd42c08102921b4a50a09b3a18a3875e3121a
Łukasz Jasiński <windowsmemphis@gmail.com>
2022-07-13 17:30:59 +0200

    make universal injector

---
+++ src/adminTool/Injector.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { addMissionDB, addQuestsDB, addItemsDB  } from '../firebase/firestore'
+import { missions } from '../firebase/missionTemplate'
+import { quests } from '../firebase/questTemplate'
+import { items } from '../firebase/itemTemplate'
+
+
+export const Injector = () => {
+
+    const injectMission = () => addMissionDB(missions[0],"4")
+    const injectItems = () =>addItemsDB(items)
+    const injectQuests = () =>addQuestsDB(quests)
+    return (
+        <button onClick={injectMission}>inject</button>
+    )
+}
