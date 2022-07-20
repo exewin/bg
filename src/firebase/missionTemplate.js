a49771c3f2871bdbb4eb6b4aa9df3c83ec12faec
Łukasz Jasiński <windowsmemphis@gmail.com>
2022-07-20 19:44:47 +0200

    add new missions to template

--- src/firebase/missionTemplate.js
+++ src/firebase/missionTemplate.js
@@ -15,4 +15,45 @@ export const missions = [
         gold: 33,
         time: 33,
     },
+    {
+        //2
+        name: "Undertaker",
+        description:"Hey, wanna make some coin, lad? I got night shift at graveyard and I need someone to hold torch. We gotta dig some new holes before dawn.",
+        xp: 260,
+        gold: 12,
+        time: 20,
+    },
+    {
+        //3
+        name: "Fisherman's Fate",
+        description:"I'm in need of your assistance, %c. My friend didn't return from fishing last night. Please, look for him at Purple Lake.",
+        xp: 512,
+        gold: 16,
+        time: 32,
+    },
+    {
+        //4
+        name: "Naturalist",
+        description:"Pardon me, traveler. I'm writing a book about all cyan flowers in Algos. Would you find some time to protect me from wild animals?",
+        xp: 490,
+        gold: 30,
+        time: 42,
+    },
+    {
+        //5
+        name: "Voodoo Master",
+        description:"Hello, mon. See dis money? If ya want it, follow dat map and bring me holy water. I be thinkin ya be good for dis venture. Be seein' ya, mon.",
+        xp: 399,
+        gold: 39,
+        time: 39,
+    },
+    
+    {
+        //6
+        name: "Travelling Circus",
+        description:"Don't look like that at me! I may be a clown, but I need a drink like everyone. My mascots have been stolen. Can you track these pranksters and teach them a lesson?",
+        xp: 300,
+        gold: 14,
+        time: 27,
+    },
 ]
